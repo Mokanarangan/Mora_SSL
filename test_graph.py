455c9a8741f4f9fc0a439a7f4c15dc4068c8b781
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-04-27 17:00:43 +0530

    setup

--- test_graph.py
+++ test_graph.py
@@ -7,3 +7,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     '--dataset', help='Location of word dataset files', type=str, required='true'
 )
+
+args = parser.parse_args
+pmi = PMI(args)
