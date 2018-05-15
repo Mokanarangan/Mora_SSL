cefbd52f8815407ce407b98b24a54bb656d9d0ea
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-15 17:52:34 +0530

    setup

--- classify.py
+++ classify.py
@@ -60,7 +60,7 @@ class Classifier():
                 index = ngram_dict[node]['index']
                 if(ngram_dict[node]['train']):
                     x_train.append(index)
-                    print(ngram_dict[node]['token'], ngram_dict[node]['tag'])
+                    # print(ngram_dict[node]['token'], ngram_dict[node]['tag'])
                 elif(ngram_dict[node]['test']):
                     x_test.append(index)
                 for connected in self.graph[node]:
