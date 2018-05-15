69cf285526af549189a02b29a9e1fe9b05cbd3cd
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-15 17:46:13 +0530

    setup

--- classify.py
+++ classify.py
@@ -2,6 +2,7 @@
 """
 from scipy.sparse import lil_matrix
 from collections import defaultdict
+from label_propagation import LGC
 
 
 class Classifier():
@@ -17,7 +18,8 @@ class Classifier():
         self.dataset = dataset
         self.BIO = BIO
         self.graph = self._process_graph('./data/' + dataset + '/graph.txt')
-        self.train = self._process_info('./data/' + dataset + '/train.txt')
+        self.train = self._process_info(
+            './data/' + dataset + '/train.txt', False, True)
         self.test = self._process_info('./data/' + dataset + '/test.txt', True)
         self.un_labeled = self._process_info(
             './data/' + dataset + '/un_labeled.txt')
@@ -38,20 +40,30 @@ class Classifier():
             x2 = total[ind]['token']
             tag = total[ind]['tag']
             test = total[ind]['test']
+            train = total[ind]['test']
             if(ind < len(total) - 1):
                 x3 = total[ind + 1]['token']
             else:
                 x3 = '<new>'
             ngram = ' '.join([x1, x2, x3])
             ngram_dict[ngram] = {'index': ind,
-                                 'token': x2, 'tag': tag, 'test': test}
+                                 'token': x2, 'tag': tag, 'test': test, 'train': train}
             ngram_index_dict[ind] = {'tag': tag, 'token': 'x2'}
-
+        x_train = []
+        y_train = []
+        x_test = []
+        y_test = []
         for node in self.graph:
             if(node in ngram_dict):
                 index = ngram_dict[node]['index']
+                if(ngram_dict[node]['train']):
+                    x_train.append(index)
+                elif(ngram_dict[node]['test']):
+                    x_test.append(index)
                 for connected in self.graph[node]:
                     Graph[index, ngram_dict[connected]['index']] = 1
+        print(len(x_train), len(x_test))
+        clf = LGC(graph=Graph, max_iter=1000)
 
     def _process_graph(self, file_name):
         """Process the created in the graph file
@@ -67,7 +79,7 @@ class Classifier():
                     graph_dict[node].append(ngram.replace('\n', ''))
         return graph_dict
 
-    def _process_info(self, file_name, test=False):
+    def _process_info(self, file_name, test=False, train=False):
         """Process data and stores in variable.
         File should be in 'X X' format
         Arguments:
@@ -80,7 +92,8 @@ class Classifier():
             split = line.split()
             tag = None
             if line in ['\n', '\r\n']:
-                data.append({'token': '<new>', 'tag': tag, 'test': test})
+                data.append({'token': '<new>', 'tag': tag,
+                             'test': test, 'train': train})
                 continue
             if len(split) > 1:
                 if(self.BIO and split[1] != 'O'):
@@ -88,5 +101,5 @@ class Classifier():
                 else:
                     tag = split[1]
             data.append(
-                {'token': split[0], 'tag': tag, 'test': test})
+                {'token': split[0], 'tag': tag, 'test': test, 'train': train})
         return data
