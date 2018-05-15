32e2b857bf7da366f51ba43cd4d7e7509486016b
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-15 19:13:18 +0530

    setup

--- classify.py
+++ classify.py
@@ -99,11 +99,11 @@ class Classifier():
         clf.fit(np.array(x_train), np.array(y_train))
         y_predict = clf.predict(np.array(x_test))
         print(ngram_index_dict[20031])
-        for ind in x_train:
-            print(ngram_index_dict[ind])
+        # for ind in x_train:
+        # print(ngram_index_dict[ind])
         # for ind in range(0, len(x_test)):
-            # elm = ngram_index_dict[x_test[ind]]
-            # print(elm['token'], elm['tag'], tag_inv_dict[y_predict[ind]])
+        # elm = ngram_index_dict[x_test[ind]]
+        # print(elm['token'], elm['tag'], tag_inv_dict[y_predict[ind]])
 
     def _process_graph(self, file_name):
         """Process the created in the graph file
