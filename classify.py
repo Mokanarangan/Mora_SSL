7ca2f20a917cd271f1443939041fa2ca019c34f8
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-15 18:12:54 +0530

    setup

--- classify.py
+++ classify.py
@@ -77,7 +77,7 @@ class Classifier():
         y_predict = clf.predict(np.array(x_test))
         for ind in x_test:
             elm = ngram_index_dict[ind]
-            # print(elm['token'], elm['tag'])
+            print(elm['token'], elm['tag'])
 
     def _process_graph(self, file_name):
         """Process the created in the graph file
