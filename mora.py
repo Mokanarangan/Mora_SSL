6b409d47d9375e33b9465991e5c06861ff969d52
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-16 17:36:24 +0530

    setup

--- mora.py
+++ mora.py
@@ -2,6 +2,7 @@
 """
 from graph import Graph
 import logging
+import numpy as np
 from utils.preprocessing import readEmbeddings, save_obj, load_obj
 
 
@@ -16,10 +17,42 @@ class Mora(Graph):
         logging.info('Initiating build vector graph..')
         final = self.train + self.test + self.un_labeled
         concat_list = self.find_ngrams(final, window)
-        for ngram in concat_list:
+        tag_dict = dict()
+        tag_inv_dict = dict()
+        tag_count = 0
+
+        x_train = []
+        y_train = []
+        embedding_list = []
+
+        ngram_dict = dict()
+
+        for i in range(0, len(concat_list)):
+            ngram = concat_list[i]
+            pointer = ngram[1]
+            embedding = None
             for elm in ngram:
                 token = elm['token']
-                tag = elm['tag']
+                # Mean or concatenation
+                if embedding is None:
+                    embedding = self._get_embeddings(token)
+                else:
+                    embedding = np.add(embedding, self._get_embeddings(token))
+            embedding = np.divide(embedding, 3)
+            tag = pointer['tag']
+
+            if(tag != None):
+                if(tag not in tag_dict):
+                    tag_dict[tag] = tag_count
+                    tag_inv_dict[tag_count] = tag
+                    tag_count += 1
+            test = pointer['test']
+            train = pointer['train']
+            if train:
+                x_train.append(embedding)
+                y_train.append(tag_dict[tag])
+            ngram_dict[i] = ngram
+            embedding_list.append(embedding)
 
     def _get_embeddings(self, token):
         if token not in self.word2Idx:
