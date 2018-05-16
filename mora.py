9c012e0f55f502d81405e684b5a9a3f0c1b2f460
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-16 11:56:33 +0530

    setup

--- mora.py
+++ mora.py
@@ -1,13 +1,14 @@
 """Defines our Mora SSL model
 """
 from graph import Graph
+from utils.preprocessing import readEmbeddings
 
 
 class Mora(Graph):
 
     def __init__(self, dataset, embedding_file):
         super().__init__(dataset)
-        print('Loading Embeddings')
+        readEmbeddings(embedding_file)
 
     def build_graph(self):
         print('Initiating build vector graph..')
