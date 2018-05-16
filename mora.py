225a63744b2ba42bbd56f6d7d51058ce20d3394b
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-16 12:01:51 +0530

    setup

--- mora.py
+++ mora.py
@@ -10,6 +10,7 @@ class Mora(Graph):
     def __init__(self, dataset, embedding_file):
         super().__init__(dataset)
         embeddings, word2Idx = readEmbeddings(embedding_file)
+        print(embeddings)
 
     def build_graph(self):
         logging.info('Initiating build vector graph..')
