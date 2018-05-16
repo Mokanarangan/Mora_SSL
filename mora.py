11770cdcce2af83550203ae0be25aba488a4fce7
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-16 11:37:41 +0530

    setup

--- mora.py
+++ mora.py
@@ -6,7 +6,7 @@ from graph import Graph
 class Mora(Graph):
 
     def __init__(self, dataset, embedding_file):
-        super(dataset)
+        super().__init__(dataset)
         print('Loading Embeddings')
 
     def build_graph(self):
