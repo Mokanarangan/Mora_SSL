9840c07a19dc97a390d80f99555bd095f61f6abd
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-16 13:20:45 +0530

    setup

--- mora.py
+++ mora.py
@@ -19,7 +19,6 @@ class Mora(Graph):
             for elm in ngram:
                 token = elm['token']
                 tag = elm['tag']
-                print(token, tag)
                 self._get_embeddings(token)
 
     def _get_embeddings(self, token):
