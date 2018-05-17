5cdb127fef0004fd360bcd080f1f63c232fac901
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-17 17:57:12 +0530

    setup

--- classify.py
+++ classify.py
@@ -87,8 +87,7 @@ class Classifier():
                     Graph[node_ind, n_gram_dict[ngram]] = 1
                     Graph[n_gram_dict[ngram], node_ind] = 1
         clf = LGC(graph=Graph, max_iter=1000)
-        print(Y_train)
-
+        print(X_test, file=fl)
         clf.fit(np.array(X_train), np.array(Y_train))
         y_predict = clf.predict(np.array(X_test))
         print(y_predict, file=fl)
