a5b739d8d4d3f59b0fa537544f85d91cb2a554d6
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-17 16:04:49 +0530

    setup

--- classify.py
+++ classify.py
@@ -101,6 +101,7 @@ class Classifier():
                 for ind in range(0, len(index_arr)):
                     index = index_arr[ind]
                     if(train_arr[ind]):
+                        print(node)
                         x_train.append(index)
                         y_train.append(tag_arr[ind])
                     elif(test_arr[ind]):
