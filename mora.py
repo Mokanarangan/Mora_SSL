9eb207e7f0d48d40a5136efb9c723b05b7aeec85
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-17 15:01:59 +0530

    setup

--- mora.py
+++ mora.py
@@ -79,7 +79,7 @@ class Mora(Graph):
             for i in range(0, len(similarity_chunk)):
                 arr = np.argsort(similarity_chunk[i])[:6]
                 temp = []
-                for j in arr:
+                for j in arr and j in ngram_dict:
                     temp.append(ngram_dict[j]['ngram'])
                 connected_vertices[ngram_dict[i + chunk_start]['ngram']] = temp
         logging.info('Drawing graph')
