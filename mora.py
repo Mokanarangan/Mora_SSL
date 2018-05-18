3a8962681371f6b96f59ab228058a7529454b0d6
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-05-18 10:56:22 +0530

    setup

--- mora.py
+++ mora.py
@@ -64,11 +64,11 @@ class Mora(Graph):
             ngram_dict[i] = {'ngram': word_comb}
             embedding_list.append(embedding)
         logging.info('Transforming vector..')
-        ann = AnnoyIndex(len(embedding_list[0]) * 3, metric='euclidean')
         clf = LinearDiscriminantAnalysis()
         clf.fit(np.matrix(x_train), np.array(y_train))
         embedding_list = clf.transform(np.array(embedding_list))
         logging.info('Building Graph')
+        ann = AnnoyIndex(embedding_list[0].shape[0], metric='euclidean')
 
         connected_vertices = dict()
 
